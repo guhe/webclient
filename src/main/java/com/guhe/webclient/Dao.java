d6a5da290a06ecedef0ff4a6e82f8c51587e598b
fanchanghu <86736264@qq.com>
2016-12-04 18:46:17 +0800

    refact view data

--- src/main/java/com/guhe/webclient/Dao.java
+++ src/main/java/com/guhe/webclient/Dao.java
@@ -9,11 +9,13 @@ import java.util.stream.Collectors;
 public enum Dao {
 	instance;
 
-	private Map<String, Portfolio> portfolios = new LinkedHashMap<>();
-	private Map<String, List<HoldingStock>> holdingstocks = new LinkedHashMap<>();
+	private Map<String, PortfolioViewData> portfolios = new LinkedHashMap<>();
+	private Map<String, List<HoldingStockViewData>> holdingstocks = new LinkedHashMap<>();
 
 	private Dao() {
-		Portfolio portfolio = new Portfolio("P00000001", "范昌虎测试组合1");
+		PortfolioViewData portfolio = new PortfolioViewData();
+		portfolio.setId("P00000001");
+		portfolio.setName("范昌虎测试组合1");
 		portfolio.setNetWorthPerUnit(1.0235);
 		portfolio.setRateOfReturnYear(0.3556);
 		portfolio.setTotalWorth(96986);
@@ -25,7 +27,9 @@ public enum Dao {
 		portfolios.put("P00000001", portfolio);
 		holdingstocks.put("P00000001", new ArrayList<>());
 
-		Portfolio portfolio2 = new Portfolio("P00000002", "范昌虎测试组合2");
+		PortfolioViewData portfolio2 = new PortfolioViewData();
+		portfolio2.setId("P00000002");
+		portfolio2.setName("范昌虎测试组合2");
 		portfolio2.setNetWorthPerUnit(1.0235);
 		portfolio2.setRateOfReturnYear(0.3556);
 		portfolio2.setTotalWorth(96986);
@@ -37,7 +41,7 @@ public enum Dao {
 		portfolios.put("P00000002", portfolio2);
 		holdingstocks.put("P00000002", new ArrayList<>());
 		
-		HoldingStock stock1 = new HoldingStock();
+		HoldingStockViewData stock1 = new HoldingStockViewData();
 		stock1.setName("平安银行");
 		stock1.setCode("000001");
 		stock1.setAmount(4000);
@@ -49,7 +53,7 @@ public enum Dao {
 		stock1.setProportion(0.366);
 		holdingstocks.get("P00000001").add(stock1);
 
-		HoldingStock stock2 = new HoldingStock();
+		HoldingStockViewData stock2 = new HoldingStockViewData();
 		stock2.setName("中国平安");
 		stock2.setCode("601318");
 		stock2.setAmount(1200);
@@ -62,15 +66,15 @@ public enum Dao {
 		holdingstocks.get("P00000001").add(stock2);
 	}
 
-	public List<Portfolio> getPortfolios() {
+	public List<PortfolioViewData> getPortfolios() {
 		return portfolios.values().stream().collect(Collectors.toList());
 	}
 
-	public Portfolio getPortfolio(String id) {
+	public PortfolioViewData getPortfolio(String id) {
 		return portfolios.get(id);
 	}
 
-	public List<HoldingStock> getHoldingStocks(String portfolioId) {
+	public List<HoldingStockViewData> getHoldingStocks(String portfolioId) {
 		return holdingstocks.get(portfolioId);
 	}
 }
--- src/test/java/com/guhe/webclient/test/PortfoliosResourceTest.java
+++ src/test/java/com/guhe/webclient/test/PortfoliosResourceTest.java
@@ -1,23 +1,23 @@
 package com.guhe.webclient.test;
 
-import static org.junit.Assert.*;
-
-import java.util.List;
+import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.core.Application;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
-import com.guhe.util.Reflector;
-import com.guhe.webclient.HoldingStock;
-import com.guhe.webclient.Portfolio;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.guhe.webclient.PortfoliosResource;
 
 public class PortfoliosResourceTest extends JerseyTest {
+
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@Override
 	protected Application configure() {
@@ -26,50 +26,54 @@ public class PortfoliosResourceTest extends JerseyTest {
 
 	@Test
 	public void test_get_portfolio_id() {
-		Portfolio actual = target("/Portfolios/P00000001").request().accept(MediaType.APPLICATION_JSON)
-				.get(Portfolio.class);
-		Portfolio expect = new Portfolio("P00000001", "范昌虎测试组合1");
-		expect.setNetWorthPerUnit(1.0235);
-		expect.setRateOfReturnYear(0.3556);
-		expect.setTotalWorth(96986);
-		expect.setProjectedLiabilities(132.7);
-		expect.setNetWorth(96853.3);
-		expect.setStockNetWorth(73787.3);
-		expect.setCash(23066);
-		expect.setProportionOfStock(0.76);
-		assertTrue(Reflector.isAllFieldsEquals(expect, actual));
+		JsonNode actual = target("/Portfolios/P00000001").request().accept(MediaType.APPLICATION_JSON)
+				.get(JsonNode.class);
+
+		ObjectNode expect = mapper.createObjectNode();
+		expect.set("id", mapper.getNodeFactory().textNode("P00000001"));
+		expect.set("name", mapper.getNodeFactory().textNode("范昌虎测试组合1"));
+		expect.set("netWorthPerUnit", mapper.getNodeFactory().numberNode(1.0235));
+		expect.set("rateOfReturnYear", mapper.getNodeFactory().numberNode(0.3556));
+		expect.set("totalWorth", mapper.getNodeFactory().numberNode(96986.0));
+		expect.set("projectedLiabilities", mapper.getNodeFactory().numberNode(132.7));
+		expect.set("netWorth", mapper.getNodeFactory().numberNode(96853.3));
+		expect.set("stockNetWorth", mapper.getNodeFactory().numberNode(73787.3));
+		expect.set("cash", mapper.getNodeFactory().numberNode(23066.0));
+		expect.set("proportionOfStock", mapper.getNodeFactory().numberNode(0.76));
+
+		assertEquals(expect, actual);
 	}
 
 	@Test
 	public void test_get_portfolio_id_holdingstocks() {
-		List<HoldingStock> actual = target("/Portfolios/P00000001/HoldingStocks").request()
-				.accept(MediaType.APPLICATION_JSON).get(new GenericType<List<HoldingStock>>() {
-				});
+		JsonNode actual = target("/Portfolios/P00000001/HoldingStocks").request().accept(MediaType.APPLICATION_JSON)
+				.get(JsonNode.class);
 
-		assertEquals(2, actual.size());
+		ArrayNode expect = mapper.createArrayNode();
+		ObjectNode obj1 = mapper.createObjectNode();
+		obj1.set("name", mapper.getNodeFactory().textNode("平安银行"));
+		obj1.set("code", mapper.getNodeFactory().textNode("000001"));
+		obj1.set("amount", mapper.getNodeFactory().numberNode(4000));
+		obj1.set("curPrice", mapper.getNodeFactory().numberNode(8.88));
+		obj1.set("marketWorth", mapper.getNodeFactory().numberNode(35520.0));
+		obj1.set("estimatedCommission", mapper.getNodeFactory().numberNode(56.23));
+		obj1.set("estimatedTax", mapper.getNodeFactory().numberNode(10.0));
+		obj1.set("netWorth", mapper.getNodeFactory().numberNode(35453.77));
+		obj1.set("proportion", mapper.getNodeFactory().numberNode(0.366));
+		expect.add(obj1);
 
-		HoldingStock expect1 = new HoldingStock();
-		expect1.setName("平安银行");
-		expect1.setCode("000001");
-		expect1.setAmount(4000);
-		expect1.setCurPrice(8.88);
-		expect1.setMarketWorth(35520);
-		expect1.setEstimatedCommission(56.23);
-		expect1.setEstimatedTax(10);
-		expect1.setNetWorth(35453.77);
-		expect1.setProportion(0.366);
-		assertTrue(Reflector.isAllFieldsEquals(expect1, actual.get(0)));
+		ObjectNode obj2 = mapper.createObjectNode();
+		obj2.set("name", mapper.getNodeFactory().textNode("中国平安"));
+		obj2.set("code", mapper.getNodeFactory().textNode("601318"));
+		obj2.set("amount", mapper.getNodeFactory().numberNode(1200));
+		obj2.set("curPrice", mapper.getNodeFactory().numberNode(32.0));
+		obj2.set("marketWorth", mapper.getNodeFactory().numberNode(38400.0));
+		obj2.set("estimatedCommission", mapper.getNodeFactory().numberNode(55.97));
+		obj2.set("estimatedTax", mapper.getNodeFactory().numberNode(10.5));
+		obj2.set("netWorth", mapper.getNodeFactory().numberNode(38333.53));
+		obj2.set("proportion", mapper.getNodeFactory().numberNode(0.396));
+		expect.add(obj2);
 
-		HoldingStock expect2 = new HoldingStock();
-		expect2.setName("中国平安");
-		expect2.setCode("601318");
-		expect2.setAmount(1200);
-		expect2.setCurPrice(32.00);
-		expect2.setMarketWorth(38400);
-		expect2.setEstimatedCommission(55.97);
-		expect2.setEstimatedTax(10.5);
-		expect2.setNetWorth(38333.53);
-		expect2.setProportion(0.396);
-		assertTrue(Reflector.isAllFieldsEquals(expect2, actual.get(1)));
+		assertEquals(expect, actual);
 	}
 }
