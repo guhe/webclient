dcea106e965eaf8ae291fa38d60bc869654b492f
fanchanghu <86736264@qq.com>
2017-01-31 12:09:56 +0800

    refact get stock data

--- src/main/java/com/guhe/webclient/TencentStockMarket.java
+++ src/main/java/com/guhe/webclient/TencentStockMarket.java
@@ -1,33 +1,31 @@
 package com.guhe.webclient;
 
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.ws.rs.client.ClientBuilder;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TencentStockMarket implements StockMarket {
 	private static final Logger LOGGER = Logger.getLogger(TencentStockMarket.class.getName());
 
 	private Map<String, StockData> cache = new HashMap<>();
 
-	public TencentStockMarket(){
+	public TencentStockMarket() {
 	}
-	
+
 	@Override
 	public synchronized StockData getStockData(String stockCode) {
 		if (!cache.containsKey(stockCode)) {
-			String url = "http://qt.gtimg.cn/q=";
-			if (stockCode.charAt(0) == '6') {
-				url += "sh" + stockCode;
-			} else if (stockCode.charAt(0) == '0' || stockCode.charAt(0) == '3') {
-				url += "sz" + stockCode;
-			} else {
-				throw new RuntimeException("unknown stock code : " + stockCode);
-			}
+			String stockFullCode = toStockFullCode(stockCode);
+			String url = toUrl(stockFullCode);
 
 			String data = ClientBuilder.newClient().target(url).request().get(String.class);
-			StockData sd = buildStockData(data);
+			StockData sd = buildStockData(data, stockFullCode);
 			cache.put(stockCode, sd);
 
 			LOGGER.info("Loaded a stock data, stock : " + stockCode + ", data : " + sd);
@@ -36,15 +34,34 @@ public class TencentStockMarket implements StockMarket {
 		return cache.get(stockCode);
 	}
 
-	private StockData buildStockData(String data) {
-		data = data.substring(data.indexOf('"') + 1, data.lastIndexOf('"'));
-		String[] substrs = data.split("~");
+	private String toStockFullCode(String stockCode) {
+		if (stockCode.charAt(0) == '6') {
+			return "sh" + stockCode;
+		} else if (stockCode.charAt(0) == '0' || stockCode.charAt(0) == '3') {
+			return "sz" + stockCode;
+		} else {
+			throw new RuntimeException("unknown stock code : " + stockCode);
+		}
+	}
 
-		StockData sd = new StockData();
-		sd.price = Double.parseDouble(substrs[3]);
-		sd.pe = Double.parseDouble(substrs[39]);
-		sd.pb = Double.parseDouble(substrs[46]);
-		return sd;
+	private String toUrl(String stockFullCode) {
+		String urlPattern = "http://web.ifzq.gtimg.cn/appstock/app/fqkline/get?param={0},day,,,1,";
+		return MessageFormat.format(urlPattern, stockFullCode);
+	}
+
+	private StockData buildStockData(String dataStr, String stockFullCode) {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode dataNode = mapper.readTree(dataStr);
+			dataNode = dataNode.get("data").get(stockFullCode).get("qt").get(stockFullCode);
+			StockData sd = new StockData();
+			sd.price = dataNode.get(3).asDouble();
+			sd.pe = dataNode.get(39).asDouble();
+			sd.pb = dataNode.get(46).asDouble();
+			return sd;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
