3a490273de003eb163cd681cc22deda50205e9d9
fanchanghu <86736264@qq.com>
2019-06-06 22:26:43 +0800

    add hongkong exchange

--- src/main/java/com/guhe/market/Exchange.java
+++ src/main/java/com/guhe/market/Exchange.java
@@ -6,12 +6,14 @@ import java.util.List;
 public enum Exchange {
 
 	ShangHai(MoneyName.RMB, '6'), ShenZheng(MoneyName.RMB, '0', '3'), ShangHai_B(MoneyName.USD,
-			'9'), ShenZheng_B(MoneyName.HKD, '2'), HuShen_CB(MoneyName.RMB, '1');
+			'9'), ShenZheng_B(MoneyName.HKD, '2'), HuShen_CB(MoneyName.RMB, '1'), HongKong(MoneyName.HKD, '0');
 
 	private static final double RATE_BROKERAGE_A = 0.00025;
 	private static final double RATE_BROKERAGE_B = 0.0005;
+	private static final double RATE_BROKERAGE_HK = 0.0003;
 	private static final double RATE_BROKERAGE_CB = 0.00005;
 	private static final double RATE_SETTLEMENT_B = 0.0005;
+	private static final double RATE_SETTLEMENT_HK = 0.0011; // tax here
 	private static final double RATE_TRANSFER = 0.00002;
 	private static final double RATE_TAX = 0.001;
 
@@ -34,6 +36,8 @@ public enum Exchange {
 			return Math.max(1, amount * RATE_BROKERAGE_B);
 		} else if (this == Exchange.ShenZheng_B) {
 			return Math.max(5, amount * RATE_BROKERAGE_B);
+		} else if (this == Exchange.HongKong) {
+			return Math.max(5, amount * RATE_BROKERAGE_HK);
 		}  else if (this == Exchange.HuShen_CB) {
 			return RATE_BROKERAGE_CB * amount;
 		} else {
@@ -44,6 +48,8 @@ public enum Exchange {
 	public double getSettlementFee(double amount) {
 		if (this == Exchange.ShangHai_B || this == Exchange.ShenZheng_B) {
 			return RATE_SETTLEMENT_B * amount;
+		} else if (this == Exchange.HongKong) {
+			return RATE_SETTLEMENT_HK * amount;
 		}
 		return 0;
 	}
@@ -58,6 +64,8 @@ public enum Exchange {
 	public double getTax(double amount) {
 		if (this == Exchange.HuShen_CB) {
 			return 0;
+		} else if (this == Exchange.HongKong) { // already contains in settlement fee
+			return 0;
 		}
 		return amount * RATE_TAX;
 	}
@@ -65,6 +73,9 @@ public enum Exchange {
 	public static Exchange instance(String stockCode) {
 		if (stockCode == null || stockCode.length() == 0) {
 			throw new RuntimeException();
+		}
+		if(stockCode.length() == 5) {
+			return Exchange.HongKong;
 		}
 		for (Exchange ex : values()) {
 			if (ex.prefixChars.contains(stockCode.charAt(0))) {
